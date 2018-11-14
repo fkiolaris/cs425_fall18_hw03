2f079e0c24878b0c8a28761a6d75040dddbc8295
fkiolaris <nicolas-95@live.com>
2018-11-14 22:32:42 +0200

    second commit

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <!DOCTYPE html>
+<?php
+// Start the session
+session_start(); ?>
 
 <html lang="en">
 <head>
@@ -15,45 +18,75 @@
 
 
 <?php
-// define variables and set to empty values
-$flag1 = true;
-$flag2 = false;
+
+
 $ans1 = $ans2 = $ans3 = $ans4 = $ans5 = "";
-$arrayPosition = 0;
 
-$passedArray = array();
-$xml = simplexml_load_file('questions.xml');
-foreach ($xml->note as $data){
-  $passedArray[] = $data;
-}
-shuffle($passedArray);
-$arraySize = count($passedArray);
+$_SESSION["flag1"] =  true;
+$_SESSION["flag2"] =  false;
+$finish = false;
+$answers = array();
+$nextTruthAns = 0;
+$truthAns = false;
+$userAns = false;
 
-if (isset($_POST['start'])) {
-  $flag1 = false;
-  $flag2 = true;
-  // $ans1 = "ans1";
-  // $ans2 = "ans2";
-  // $ans3 = "ans3";
-  // $ans4 = "ans4";
-  // $ans5 = "ans5";
+if (isset($_GET['start'])) {
+  $_SESSION["flag1"] =  false;
+  $_SESSION["flag2"] =  true;
+  $_SESSION["answers"] = $answers;
+
+  $passedArray = array();
+  $xml = simplexml_load_file('questions.xml');
+  foreach ($xml->note as $data){
+    $passedArray[] = $data;
+  }
+  shuffle($passedArray);
+  $arraySize = count($passedArray);
+  $_SESSION["array_size"] = $arraySize;
+  $objectArray = json_encode($passedArray);
+
+  $_SESSION["array"] =  $objectArray;
+
   $arrayPosition = 0;
+  $_SESSION["array_position"] = $arrayPosition;
   $ans1 = $passedArray[$arrayPosition]->all_answers->answer[0];
   $ans2 = $passedArray[$arrayPosition]->all_answers->answer[1];
   $ans3 = $passedArray[$arrayPosition]->all_answers->answer[2];
   $ans4 = $passedArray[$arrayPosition]->all_answers->answer[3];
   $ans5 = $passedArray[$arrayPosition]->all_answers->answer[4];
+  $nextTruthAns = $passedArray[$arrayPosition]->answer;
 }
 
 if (isset($_POST['next'])) {
-  $flag1 = false;
-  $flag2 = true;
+  $_SESSION["flag1"] =  false;
+  $_SESSION["flag2"] =  true;
+  $arraySize = $_SESSION["array_size"];
+  $arrayPosition =  $_SESSION["array_position"];
+  $answers = $_SESSION["answers"];
   $arrayPosition ++;
-  $ans1 = $passedArray[$arrayPosition]->all_answers->answer[0];
-  $ans2 = $passedArray[$arrayPosition]->all_answers->answer[1];
-  $ans3 = $passedArray[$arrayPosition]->all_answers->answer[2];
-  $ans4 = $passedArray[$arrayPosition]->all_answers->answer[3];
-  $ans5 = $passedArray[$arrayPosition]->all_answers->answer[4];
+
+  if ($arraySize > $arrayPosition){
+ 
+    $passedArray = json_decode($_SESSION["array"]);
+
+    $truthAns = $_POST["truthAns"];
+    $userAns = $_POST["ans"];
+
+    $ans1 = $passedArray[$arrayPosition]->all_answers->answer[0];
+    $ans2 = $passedArray[$arrayPosition]->all_answers->answer[1];
+    $ans3 = $passedArray[$arrayPosition]->all_answers->answer[2];
+    $ans4 = $passedArray[$arrayPosition]->all_answers->answer[3];
+    $ans5 = $passedArray[$arrayPosition]->all_answers->answer[4];
+    $nextTruthAns = $passedArray[$arrayPosition]->answer;
+
+    $_SESSION["array"] = json_encode($passedArray);
+    $_SESSION["array_position"] = $arrayPosition;
+
+    if ($arraySize - 1 == $arrayPosition) $finish = true;
+
+  }else{
+    
+  }
 }
 ?>
 
@@ -80,14 +113,14 @@ if (isset($_POST['next'])) {
 <br>
 
 <?php
-if ($flag1) {
+if ($_SESSION["flag1"]) {
 ?>
 
 <div class="container">
   
   <div class = "container_backround pagination-centered text-center" >
   <h1 id="welcome_message"><b>Welcome to quizzer!!</b></h1>
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
       <input type="submit" id="startButton" value="Start Game" name="start">
     </form>
   </div>
@@ -98,7 +131,7 @@ if ($flag1) {
 
 
 <?php
-if ($flag2) {
+if ($_SESSION["flag2"]) {
 ?>
 
 <div id="container_position" class="container container_backround">
@@ -107,17 +140,17 @@ if ($flag2) {
     <!-- <button id="startButton">Start Game</button> -->
     <form  method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
       <div class="row">
-      <h2 name="quest"><?php echo $passedArray[$arrayPosition]->question ?></h2>
+      <h2 name="quest"><?php echo json_decode($_SESSION["array"])[$_SESSION["array_position"]]->question ?></h2>
       <div class="align_right_top">
-        <span><?php echo $arrayPosition; ?></span>
+        <span><?php echo ($_SESSION["array_position"] +1 ); ?></span>
       <span><?php echo "/" ?></span>
-      <span><?php echo $arraySize; ?></span>
+      <span><?php echo $_SESSION["array_size"]; ?></span>
     </div>
     </div>
-
+      <input type="hidden"  name="truthAns" value = <?php echo $nextTruthAns ?>>
       <div class="row">
       <div class="col-25">
-      <input type="radio" id="radio1" name="ans" value="Next">
+      <input type="radio" id="radio1" name="ans" value = 0 checked >
       <label for=radio1 ><?php echo $ans1 ?></label>
       </div>
 
@@ -125,35 +158,40 @@ if ($flag2) {
 
     <div class="row">
     <div class="col-25">
-      <input type="radio" id="radio2" name="ans" value="Next">
+      <input type="radio" id="radio2" name="ans" value = 1>     
       <label for=radio2 ><?php echo $ans2 ?></label>
       </div>
     </div>
 
     <div class="row">
     <div class="col-25">
-      <input type="radio" id="radio3" name="ans" value="Next">
+      <input type="radio" id="radio3" name="ans" value = 2>
       <label for=radio3 ><?php echo $ans3 ?></label>
       </div>
     </div>
 
     <div class="row">
     <div class="col-25">
-      <input type="radio" id="radio4" name="ans"  value="Next">
+      <input type="radio" id="radio4" name="ans"  value = 3>
       <label for=radio4 ><?php echo $ans4 ?></label>
       </div>
     </div>
 
         <div class="row">
         <div class="col-25">
-      <input type="radio" id="radio5" name="ans" value="Next">
+      <input type="radio" id="radio5" name="ans" value = 4>
       <label for=radio5 ><?php echo $ans5 ?></label>
       </div>
     </div>
 
     <div class="row">
-      <input type="submit" id="nextButton" value="Next" name="next">
-      <input type="submit" id="endButton" value="End Game">
+    <input type="submit" id="endButton" value="End Game">
+     
+      <?php if ($finish) {  ?>
+        <input type="submit" id="finishButton" value="Finish" name="finish">
+      <?php } else { ?>
+        <input type="submit" id="nextButton" value="Next" name="next">
+      <?php } ?>
     </div>
 
     </form>
@@ -166,5 +204,18 @@ if ($flag2) {
 </div>
 </div>
 
+<?php     if ($truthAns == $userAns){
+     echo "<script>";
+     echo "alert('TRUE');";
+     echo "</script>";
+      $answers[] = true;
+    }
+    else{
+      echo "<script>";
+      echo "alert('FALSE');";
+      echo "</script>";
+      $truthAnsersArray[] = false;
+    } ?>
+    
 </body>
 </html>
