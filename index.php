585a296094701c94195034780f7ca12559c40926
fkiolaris <nicolas-95@live.com>
2018-11-15 20:08:20 +0200

    fourth commit

--- index.php
+++ index.php
@@ -13,20 +13,18 @@ session_start(); ?>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body class="main_page_backround">
 
-
-<?php
+<?php 
 
 function findNext($array, $curPos, $diff, $moreDifficulty){
   $counter = 0;
   $size = count($array);
   
   $nextDifficulty = 0;
-    //    echo "<script>";
-    //  echo "alert($diff);";
-    //  echo "</script>";
 
   if ($moreDifficulty) {
     switch($diff){
@@ -65,8 +63,11 @@ function findNext($array, $curPos, $diff, $moreDifficulty){
 
 $question = $ans1 = $ans2 = $ans3 = $ans4 = $ans5 = "";
 
-$_SESSION["flag1"] =  true;
-$_SESSION["flag2"] =  false;
+$_SESSION["flag1"] = true;
+$_SESSION["flag2"] = false;
+$_SESSION["flag3"] = false;
+define("QUESTIONS", 5);
+
 $finish = false;
 $answers = array();
 $nextTruthAns = 0;
@@ -75,8 +76,10 @@ $userAns = false;
 $difficulty = 0;
 
 if (isset($_GET['start'])) {
-  $_SESSION["flag1"] =  false;
   $_SESSION["flag2"] =  true;
+  $_SESSION["flag1"] = false;
+
+  $_SESSION["SCORE"] = 0;
   $_SESSION["answers"] = $answers;
   $_SESSION["CURRENT_ARRAY_POS"] = 0;
 
@@ -110,12 +113,12 @@ if (isset($_GET['start'])) {
   $difficulty = $element->difficulty;
 
   $_SESSION["CURRENT_ARRAY_POS"] = $position;
-  // $_SESSION["difficulty"] = $difficulty;
 }
 
-if (isset($_POST['next'])) {
-  $_SESSION["flag1"] =  false;
+if (isset($_POST['next']) || isset($_POST['finish'])) {
   $_SESSION["flag2"] =  true;
+  $_SESSION["flag1"] = false;
+
   $arraySize = $_SESSION["array_size"];
   $arrayPosition =  $_SESSION["array_position"];
   $answers = $_SESSION["answers"];
@@ -123,24 +126,26 @@ if (isset($_POST['next'])) {
   $value = "";
   $currentPosition = $_SESSION["CURRENT_ARRAY_POS"];
   $moreDifficulty;
+  $currentScore =  $_SESSION["SCORE"];
 
-  if ($arrayPosition < 9){
+  if ($arrayPosition < QUESTIONS + 1){
     
     $passedArray = json_decode($_SESSION["array"]);
     $truthAns = $_POST["truthAns"];
     $userAns = $_POST["ans"];
     $difficulty = $_POST["difficulty"];
-  
+
+    $data = new stdClass();
+    $data->level =  $difficulty;
     if ($truthAns == $userAns){
-       $answers[] = true;
+      $data->answer = true;
+       $answers[] = $data;
        $moreDifficulty = true;
-    //   echo "<script>";
-    //  echo "alert('TRUE');";
-    //  echo "</script>";
-      $answers[] = true;
+       $currentScore += $difficulty;
      }
      else{
-       $answers[] = false;
+       $data->answer = false;
+       $answers[] = $data;
        $moreDifficulty = false;
 
      }
@@ -161,18 +166,40 @@ if (isset($_POST['next'])) {
    
     $_SESSION["array_position"] = $arrayPosition;
     $_SESSION["CURRENT_ARRAY_POS"] = $position;
+    $_SESSION["answers"] = $answers;
+    $_SESSION["SCORE"] = $currentScore;
 
-    if ($arrayPosition + 1 == 9) $finish = true;
+    if ($arrayPosition + 1 == QUESTIONS) $finish = true;
 
   }else{
     
   }
+
+  if (isset($_POST['finish'])) {
+    $_SESSION["flag3"] =  true;
+    $_SESSION["flag2"] =  false;
+  }
 }
+
+$showPopup = false;
+if (isset($_POST['save'])) {
+
+  $data = new stdClass();
+  $data->nickname = $_POST['nickname'];
+  $data->score = $_SESSION["SCORE"];
+  $inp = file_get_contents('results.json');
+
+  $tempArray = json_decode($inp);
+  if ($tempArray == null) $tempArray = array();
+  array_push($tempArray, $data);
+  $jsonData = json_encode($tempArray);
+  file_put_contents('results.json', $jsonData);
+} 
 ?>
 
 
 <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#">Quizzer</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -224,7 +251,7 @@ if ($_SESSION["flag2"]) {
       <div class="align_right_top">
         <span><?php echo ($_SESSION["array_position"] +1 ); ?></span>
       <span><?php echo "/" ?></span>
-      <span><?php echo $_SESSION["array_size"]; ?></span>
+      <span><?php echo QUESTIONS ?></span>
     </div>
     </div>
       <input type="hidden"  name="truthAns" value = <?php echo $nextTruthAns ?>>
@@ -282,21 +309,76 @@ if ($_SESSION["flag2"]) {
 }
 ?>
 
-</div>
-</div>
+<?php
+if ($_SESSION["flag3"]) {
+?>
 
-<?php  //   if ($truthAns == $userAns){
-    //  echo "<script>";
-    //  echo "alert('TRUE');";
-    //  echo "</script>";
-    //   $answers[] = true;
-    // }
-    // else{
-    //   echo "<script>";
-    //   echo "alert('FALSE');";
-    //   echo "</script>";
-    //   $answers[] = false;
- //   } ?>
+<div id="container_position" class="container container_backround">
+  <div>
+  <h1><b>Save your score..</b></h1>
+    <!-- <button id="startButton">Start Game</button> -->
+    <form  method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+
+    <h2>Striped Rows</h2>
+    <p>The .table-striped class adds zebra-stripes to a table:</p>            
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Question:</th>
+          <th>Level</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+
+      <tbody>
+
+      <?php
+      $counter = 0;
+      $answers = $_SESSION["answers"];
+      foreach ($answers as $data){
+        if ($data->answer){ ?>
+          <tr>
+          <td><?php echo ++$counter ?></td>
+          <td><?php echo $data->level ?></td>
+          <td>Success</td>
+        </tr> 
+      <?php  } else { ?>
+         <tr>
+          <td><?php echo ++$counter ?></td>
+          <td><?php echo $data->level ?></td>
+          <td>Failed</td>
+        </tr> 
+      <?php }
+      } ?>
+
+      </tbody>
+
+    </table>
+
+      <div class="row">
+    <div class="col-25">
+      <label for=nickname >Your nickname:</label>
+      </div>
+      <div class="col-75">
+      <input type="text" id="nickname" name="nickname" value="nickname">     
+      </div>
+
+    </div>
+
+    <div class="row">     
+    <input type="submit" id="saveButton" value="Save" name="save">
+    <input type="submit" id="returnButton" value="Exit" name="return">
+    </div>
+
+    </form>
+  </div>
+
+<?php
+}
+?>
+
+</div>
+</div>
 
 </body>
 </html>
